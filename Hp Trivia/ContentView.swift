6006e4882ac64f003cdae95afe1b1638e3cba436
DawoodAkbar <uzumakiminato806@gmail.com>
2025-10-06 09:51:42 +0500

    Initial Commit

---
+++ Hp Trivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hp Trivia
+//
+//  Created by Dawood on 06/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hp Trivia/Hp_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hp_TriviaApp.swift
+//  Hp Trivia
+//
+//  Created by Dawood on 06/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hp_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
