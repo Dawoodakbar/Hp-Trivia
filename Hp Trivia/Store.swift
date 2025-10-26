184e9209f8b2e8d12ca350badf3375ec4fa72ce5
Dawood Akbar <uzumakiminato806@gmail.com>
2025-10-26 09:19:20 +0500

    Part17: added purchase func to verifiy purchase, also check purchase to check if the purchase was successfull and also watchUpdate func to check for updates

--- Hp Trivia/Store.swift
+++ Hp Trivia/Store.swift
@@ -27,8 +27,14 @@ class Store: ObservableObject {
     ]
     
     @Published var products: [Product] = []
+    @Published var purchaseIDs = Set<String>()
     
     private var productIDs = ["hp4", "hp5", "hp6", "hp7"]
+    private var updates: Task<Void, Never>? = nil
+    
+    init() {
+       updates = watchForUpdates()
+    }
     
     func loadProducts() async {
         do {
@@ -37,4 +43,60 @@ class Store: ObservableObject {
             print("Couldn't fetch those product: \(error)")
         }
     }
-}
+    
+    func purchase(_ product: Product) async {
+        do {
+            let result = try await product.purchase()
+            
+            switch result {
+                // Purchase succefull but now we have to verfiy the receipt
+            case .success(let verificationResult):
+                switch verificationResult {
+                case .unverified(let signedType, let verificationError):
+                    print("Error on \(signedType) and \(verificationError)")
+                    
+                case .verified(let signedType):
+                    purchaseIDs.insert(signedType.productID)
+                }
+                // User cancelled
+            case .userCancelled:
+                break
+            case .pending:
+                break
+                
+            @unknown default:
+                break
+            }
+            
+        } catch {
+            print("Couldn't purchase the product \(error)")
+        }
+    }
+        
+        func checkPurchase() async {
+            for product in products {
+                guard let state = await product.currentEntitlement else { return }
+                
+                switch state {
+                case .unverified(let signedType, let verificationError):
+                    print("Error at \(signedType), and \(verificationError)")
+                    
+                case .verified(let signedType):
+                    if signedType.revocationDate == nil {
+                        purchaseIDs.insert(signedType.productID)
+                    } else {
+                        purchaseIDs.remove(signedType.productID)
+                    }
+                }
+            }
+        }
+        
+        private func watchForUpdates() -> Task<Void, Never> {
+            Task(priority: .background) {
+                for await _ in Transaction.updates {
+                   await checkPurchase()
+                }
+            }
+        }
+    }
+
